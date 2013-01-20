c2951107733e9de11666552d071ed5785f1dc797
Brandon Toone <brandon@rdlk.co>
2013-01-19 20:15:58 -0700

    Add -e option to echo

--- installers/gems.sh
+++ installers/gems.sh
@@ -2,25 +2,26 @@
 
 # Installs utility gems for the home environment
 
-echo "\nINFO: Installing framework gems"
+echo -e "\nINFO: Installing framework gems"
 gem install rails
 
-echo "INFO: Installing developer gems"
+echo -e "\nINFO: Installing developer gems"
 gem install awesome_print
 gem install cheat
 gem install pry
 gem install pry-nav
 gem install travis
 gem install travis-lint
+gem install hl
 
-echo "INFO: Installing environment gems"
+echo -e "\nINFO: Installing environment gems"
 gem install rake
 gem install tmuxinator
 
-echo "INFO: Installing provisioning gems"
+echo -e "\nINFO: Installing provisioning gems"
 gem install knife-solo
 gem install knife-github-cookbooks
 gem install knife-ec2
 gem install librarian
 
-echo "\nFINISHED: Remember to run `rbenv rehash` if using rbenv"
+echo -e "\nFINISHED: Remember to run `rbenv rehash` if using rbenv"
