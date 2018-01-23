39207b473a43ee4eddd62fcc37ee2ac9167bec99
Brandon Toone <brandon@rdlk.co>
2018-01-23 11:18:10 -0700

    Disable un-necessary brews
    Most of these aren't needed on a new setup. I've disabled to help keep things clean.

--- installers/brews.sh
+++ installers/brews.sh
@@ -6,29 +6,27 @@ brew update
 brew upgrade
 brew install git
 brew install markdown
-brew install mercurial
+# brew install mercurial
 brew install mongodb
-brew install multimarkdown
+# brew install multimarkdown
 brew install mysql
 brew install node
 brew install postgresql
-brew install rebenv
-brew install redis
-brew install ruby-build
+brew install rbenv
+# brew install redis
 brew install tmux
 brew install vcprompt
 brew install wget
-brew install corkscrew
-brew install autoconf
-brew install automake
-brew install libtool
-brew install rabbitmq
-brew install imagemagick
-brew install ssh-copy-id
-brew install ghostscript
-brew install graphviz
-brew install tig
-brew install sqlite
-brew install openssl
-brew install reattach-to-user-namespace
+# brew install corkscrew
+# brew install autoconf
+# brew install automake
+# brew install libtool
+# brew install rabbitmq
+# brew install imagemagick
+# brew install ssh-copy-id
+# brew install ghostscript
+# brew install graphviz
+# brew install tig
+# brew install openssl
+# brew install reattach-to-user-namespace
 brew install ctags
