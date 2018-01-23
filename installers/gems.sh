710babbb23555c6407f01cbd9873cf6bf1dc8476
Brandon Toone <brandon@rdlk.co>
2018-01-23 11:05:14 -0700

    Disable gem installers
    There is no need to run this script any longer. Its better to just use Homebrew and rbenv. Manually install the gems you need at the time you need them for the env you need them in.
    
    I'm keeping the gems commented out for reference on which gems are useful to me.

--- installers/gems.sh
+++ installers/gems.sh
@@ -1,29 +1,38 @@
 #!/usr/bin/env bash
 
+# Use Homebrew to install everything:
+# * git
+# * git-flow-avh
+# * rbenv
+# * tmux
+# * vcprompt
+
+# After installing rbenv, use it to install the latest version of ruby and any of the gems you'd like from the list below.
+
+# -- LEGACY -- #
+
 # Installs utility gems for the home environment
 
-echo -e "\nINFO: Installing framework gems"
-gem install rails
+# echo -e "\nINFO: Installing framework gems"
+# gem install rails
 
-echo -e "\nINFO: Installing developer gems"
-gem install awesome_print
-gem install cheat
-gem install pry
-gem install pry-nav
-gem install travis
-gem install travis-lint
-gem install hl
-gem install foreman
-gem install launchy
+# echo -e "\nINFO: Installing developer gems"
+# gem install awesome_print
+# gem install cheat
+# gem install pry
+# gem install pry-nav
+# gem install hl
+# gem install foreman
+# gem install launchy
 
-echo -e "\nINFO: Installing environment gems"
-gem install rake
-gem install tmuxinator
+# echo -e "\nINFO: Installing environment gems"
+# gem install rake
+# gem install tmuxinator
 
-echo -e "\nINFO: Installing provisioning gems"
-gem install knife-solo
-gem install knife-github-cookbooks
-gem install knife-ec2
-gem install librarian
+# echo -e "\nINFO: Installing provisioning gems"
+# gem install knife-solo
+# gem install knife-github-cookbooks
+# gem install knife-ec2
+# gem install librarian
 
-echo -e "\nFINISHED: Remember to run `rbenv rehash` if using rbenv"
+# echo -e "\nFINISHED: Remember to run `rbenv rehash` if using rbenv"
